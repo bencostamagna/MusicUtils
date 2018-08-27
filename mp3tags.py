705fb2dc1d0597a922250f3b26cdde3596724be3
Benoit Costamagna <b.costamagna@protonmail.com>
2018-08-27 13:42:42 +0200

    Adding user input and basic renaming
    User can now chose to either rename the artist folder or retag the file
    Only renaming is implemented

--- mp3tags.py
+++ mp3tags.py
@@ -21,8 +21,6 @@ class TagMismatch:
 
 
 
-
-
 def scan_files(folder, flist):
     for f in os.listdir(folder):
         if os.path.isfile(os.path.join(folder, f)):
@@ -31,25 +29,55 @@ def scan_files(folder, flist):
             scan_files(os.path.join(folder, f), flist)
 
 
+def solve_mismatch(folder, mismatch_list):
+    print(str(len(mismatch_list)) + " bad artist tags")
+    recheck = False
+    for m in mismatch_list:
+        print(str(len(m.files)) + " files containing tag "+m.tag+" but folder name is "+m.expected)
+        print("1 Rename folder to " + m.tag)
+        print("2 Change tags to " + m.expected)
+        print("3 Ignore")
+        var = input()
+        if var == "1":
+            print ("Renaming "+folder + " to "+os.path.join(os.path.dirname(folder), m.tag))
+            os.rename(folder, os.path.join(os.path.dirname(folder), m.tag))
+            recheck = True
+        elif var == "2":
+            print("Retagging")
+        else:
+            pass
+    return recheck
+
 def process_folder(folder):
     flist = []
     scan_files(folder, flist)
+    recheck = True
 
-    mismatch = None
-    for f in flist:
-        try:
-            magic_string = magic.from_file(f)
-            if 'MPEG' in magic_string:
-                tags = eyed3.load(f)
-                if tags.tag.artist != os.path.basename(folder):
-                    if mismatch == None:
-                        mismatch = TagMismatch(tag=tags.tag.artist, expected=os.path.basename(folder))
-                    mismatch.add_file(f)
-        except:
-            error_state.append(f)
-    
-    if mismatch != None:
-        artist_mismatch.append(mismatch)
+    while recheck:
+        recheck = False
+        mismatch_list = []
+        for f in flist:
+            try:
+                magic_string = magic.from_file(f)
+                if 'MPEG' in magic_string:
+                    tags = eyed3.load(f)
+                    if tags.tag.artist != os.path.basename(folder):
+                        found=False
+                        for m in mismatch_list:
+                            if m.tag == tags.tag.artist:
+                                m.add_file(f)
+                                found=True
+                        if not found:
+                            mismatch_list.append(TagMismatch(tag=tags.tag.artist, expected=os.path.basename(folder)))
+            except:
+                error_state.append(f)
+
+        if len(mismatch_list) > 0:
+            recheck = solve_mismatch(folder, mismatch_list)
+
+
+############################################################################
+
 
 if len(sys.argv) == 0:
     exit(0)
@@ -72,8 +100,5 @@ for filename in os.listdir(root):
 
 
 print(str(len(error_state)) + " errors in files")
-print(str(len(artist_mismatch)) + " bad artist tags")
-for m in artist_mismatch:
-    print(m.tag + " -> " + m.expected + " in " + str(len(m.files)) + " files")
 
 
